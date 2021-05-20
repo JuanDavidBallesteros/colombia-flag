78b3a061ccd8182429866ffc0cfbed5830225ccb
JuanDavidBallesteros <juan.ballesteros@u.icesi.edu.co>
2021-05-20 17:19:14 -0500

    code by threads

---
+++ src/ui/Main.java
@@ -0,0 +1,16 @@
+package ui;
+import model.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+
+        Flag col = new Flag(20, 37, 42, 5, 3, 3, 100);
+
+        col.paintFlag();
+        col.goFinal();
+        
+    }
+}
+
